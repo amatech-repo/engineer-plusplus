a221b439c09dd0cefde1d8a277557efdb5dbf79e
kitsune_yk <agvksl543@gmail.com>
2023-04-23 18:37:06 +0900

    feat: 最近学習した教材一覧を実装

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,11 +1,12 @@
 import Navigation from "@/components/Navigataion";
 import Card from "@/components/Card";
+import RecentStudy from "@/components/RecentStudy";
 
 const Dashboard = () => {
   return (
     <>
       <h3>最近の学習</h3>
-      <Card title="React入門" totalStudyTime={10} tag={["React", "JavaScript", "Web開発"]} />
+      <RecentStudy />
     </>
   );
 };
