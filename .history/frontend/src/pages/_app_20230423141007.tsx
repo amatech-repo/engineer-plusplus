4a75235b4c20e4d457c28b0ba4e33757e2577ed4
kitsune_yk <agvksl543@gmail.com>
2023-04-23 14:12:40 +0900

    chore: muiをインストール

---
+++ .history/frontend/src/pages/_app_20230423141007.tsx
@@ -0,0 +1,33 @@
+import "@/styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../../config/theme';
+import createEmotionCache from '../../config/createEmotionCache';
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+      </ThemeProvider>
+    </CacheProvider>
+
+  );
+}
