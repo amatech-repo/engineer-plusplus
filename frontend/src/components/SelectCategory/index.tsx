be3aabab5a4b8c0a60298ee67bad59f743531d9a
maple-fm <fuma.deguchi@gmail.com>
2023-04-25 02:57:14 +0900

    refactor: リストタイトルを付与

--- frontend/src/components/SelectCategory/index.tsx
+++ frontend/src/components/SelectCategory/index.tsx
@@ -3,24 +3,32 @@ import { useState } from 'react';
 
 const mockData = categoriesData;
 
-const CatetoryList = () => {
+interface Props {
+    listTitle: string
+}
+
+const CatetoryList = (props: Props) => {
     const [selectedCategory, setSelectedCategory] = useState('');
+    const { listTitle } = props
 
     const handleCategoryChange = (e) => {
         setSelectedCategory(e.target.value);
     };
 
     return (
-        <form>
-            <select id="category" value={selectedCategory} onChange={handleCategoryChange}>
-                <option value="">--選択してください--</option>
-                {mockData.map((category) => (
-                    <option key={category.id} value={category.name}>
-                        {category.name}
-                    </option>
-                ))}
-            </select>
-        </form>
+        <>
+            <h3>{ listTitle }</h3>
+            <form>
+                <select id="category" value={selectedCategory} onChange={handleCategoryChange}>
+                    <option value="">--選択してください--</option>
+                    {mockData.map((category) => (
+                        <option key={category.id} value={category.name}>
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+            </form>
+        </>
     );
 };
 
