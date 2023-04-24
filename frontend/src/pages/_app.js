3ca1026c6bbed4f6bef1fdd2bb870bd03171ca29
matsuo1234567 <122419523+matsuo1234567@users.noreply.github.com>
2023-04-24 09:11:18 +0900

    feat:新規登録&ログイン機能

---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,10 @@
+// ログインとユーザー登録の画面の表示がおかしくなるのでglobals.cssは読み込まない
+// import '../styles/globals.css'
+// ↓ これを追加してBootstrapで読み込む
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
