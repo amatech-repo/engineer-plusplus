176c7440cb576b3efaae122ce2bee18af7dd5fd9
AnriNakamura <an12inakamu12a@gmail.com>
2023-04-25 01:28:26 +0900

    feat: 全コンポーネントをAuthProviderでラッピングする

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,10 +1,13 @@
-// ログインとユーザー登録の画面の表示がおかしくなるのでglobals.cssは読み込まない
-// import '../styles/globals.css'
-// ↓ これを追加してBootstrapで読み込む
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthProvider } from '../context/AuthContext'
 
+// 全コンポーネントをAuthProviderでラッピングする
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
