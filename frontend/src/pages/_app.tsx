3f17297aad1f163ae48dbb15b983813738e2c400
AnriNakamura <an12inakamu12a@gmail.com>
2023-04-25 02:10:51 +0900

    feat: TypeScriptに変更

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -32,3 +32,15 @@ export default function MyApp(props: MyAppProps) {
 
   );
 }
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthProvider } from '../context/AuthContext';
+import type { AppProps } from 'next/app';
+
+// 全コンポーネントをAuthProviderでラッピングする
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
