d60c0de7521dc72f5a430af708cd1ab3c929210e
kitsune_yk <agvksl543@gmail.com>
2023-04-25 17:24:00 +0900

    story: countupを修正

--- frontend/src/components/Timer/Countup.stories.js
+++ frontend/src/components/Timer/Countup.stories.js
@@ -3,7 +3,17 @@ import Countup from './Countup';
 export default {
   title: 'Components/Countup',
   component: Countup,
+  argTypes: {
+    hour: { control: 'number' },
+    minute: { control: 'number' },
+    second: { control: 'number' },
+  },
 };
 
 const Template = (args) => <Countup {...args} />;
 export const Default = Template.bind({});
+Default.args = {
+  hour: 0,
+  minute: 0,
+  second: 0,
+};
