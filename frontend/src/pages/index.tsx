c260dde11483f0beb009e3bb8bbcbbdaa1ca8151
kitsune_yk <agvksl543@gmail.com>
2023-04-27 00:06:32 +0900

    feat: ログインしたら自動でメインに自動繊維

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 
-import Dashboard from "./Dashboard";
+import { useAuth } from "@/context/AuthContext";
+import Dashboard from "./dashboard";
 import Layout from "@/components/Layouts/layout";
+import Login from "./auth/login";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { currentUser } = useAuth();
+
   return (
     <>
       <Head>
@@ -16,9 +20,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Layout>
-          <Dashboard />
-        </Layout>
+        {currentUser ? (
+          <Layout>
+            <Dashboard />
+          </Layout>
+        ) : (
+          <Login />
+        )}
       </main>
     </>
   );
