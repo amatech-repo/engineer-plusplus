04869c265b3bfaaeedcfd2cd7683e675b0431349
kitsune_yk <agvksl543@gmail.com>
2023-04-27 13:02:17 +0900

    feat: pluginsに型定義がないためJSXを使用

--- frontend/src/components/Questions/PostPreview.jsx
+++ frontend/src/components/Questions/PostPreview.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 import './markdown.module.css'
 
-const PostPreview = (props: any) => {
+const PostPreview = (props) => {
   return (
     <div className="h-full w-full mr-10">
       <div className="markdown-preview h-full w-full border shadow-xl mb-5 rounded-xl py-4 px-2 overflow-y-scroll bg-white">
