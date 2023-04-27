9151e33e81c8c96bdbda4194934a4b29c4d833fb
kitsune_yk <agvksl543@gmail.com>
2023-04-27 12:31:14 +0900

    bugfix: sbが実行できないエラーを修正

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -20,6 +22,27 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  staticDirs: ["../out"],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: {
+              auto: true, // *.module.scssファイル全てを対象
+            },
+            url: false, // cssのbackgroundで設定した画像へのパスがプロジェクトルートからの絶対パスになるように設定
+          },
+        },
+        "sass-loader",
+      ],
+      include: path.resolve(__dirname, "../src/"),
+    });
+     return config;
   },
 };
 export default config;
