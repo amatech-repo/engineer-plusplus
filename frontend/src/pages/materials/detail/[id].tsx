6e8f9508ca441bc644c7b2b6a79658ded560103d
maple-fm <fuma.deguchi@gmail.com>
2023-04-28 15:06:13 +0900

    feat: タイマー機能の追加

--- frontend/src/pages/materials/detail/[id].tsx
+++ frontend/src/pages/materials/detail/[id].tsx
@@ -7,16 +7,67 @@ import MaterialDetailCard from "@/components/MaterialDetailCard";
 import TimeCard from "@/components/Timer/TimeCard";
 import TimerBox from "@/components/Timer/TimerBox";
 import StudyChart from "@/components/Chart";
+import { SetStateAction, useEffect, useState } from "react";
+import { Button } from "reactstrap";
 
 const MaterialDetail = () => {
+
+  const [timer, setTimer] = useState(0); // ストップウォッチの時間
+  const [isActive, setIsActive] = useState(false); // ストップウォッチが動いているかどうか
+  const [setIntervalID, setSetIntervalID] = useState(null);
+
+  useEffect(() => {
+    let intervalId: any = null;
+    if (isActive) {
+      intervalId = setInterval(() => {
+        setTimer((timer) => timer + 1);
+      }, 1000);
+      setSetIntervalID(intervalId);
+    } else if (!isActive && timer !== 0) {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId);
+  }, [isActive, timer]);
+
+  const handleStart = () => {
+    setIsActive(true);
+  };
+
+  const handleStop = () => {
+    setIsActive(false);
+    setTimer(0);
+    clearInterval(setIntervalID);
+  };
+
+  const handlePause = () => {
+    setIsActive(false);
+    clearInterval(setIntervalID);
+  }
+
   return (
     <Layout>
       <div>
         <SContainer>
           <MaterialDetailCard />
           <STimerContainer>
-            <TimeCard hour={1} minute={23} second={45} />
-            <TimerBox />
+            <TimeCard hour={Math.floor(timer / 3600)} minute={Math.floor(timer / 60)} second={timer % 60} />
+            {!isActive && (
+              <Button variant="contained" color="inherit" onClick={handleStart}>
+                START
+              </Button>
+            )}
+
+            {isActive && (
+              <Button variant="contained" color="inherit" onClick={handlePause}>
+                PAUSE
+              </Button>
+            )}
+
+            {timer > 0 && (
+              <Button variant="contained" color="inherit" onClick={handleStop}>
+                STOP
+              </Button>
+            )}
           </STimerContainer>
         </SContainer>
         <StudyChart />
@@ -36,3 +87,4 @@ const SContainer = styled.div`
 const STimerContainer = styled.div`
   height: 100%;
 `
+
