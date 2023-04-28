38a7fa1d73baef948f79de12b4bc64034c167dfd
kitsune_yk <agvksl543@gmail.com>
2023-04-28 13:43:39 +0900

    feat: 教材一覧画面に教材登録ボタンを追加

--- frontend/src/pages/materials/index.tsx
+++ frontend/src/pages/materials/index.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link";
+import styled from "styled-components";
+
 import Layout from "@/components/Layouts/layout";
 import MaterialList from "@/components/MaterialsList";
-import styled from "styled-components";
+import CustomButton from "@/components/Button";
 
 const Dashboard = () => {
   return (
     <Layout>
       <>
+        <SContainer>
+          <Link href="/materials/register">
+            <CustomButton label="教材を登録" />
+          </Link>
+        </SContainer>
         <MaterialList listTitle="教材一覧" />
         <MaterialList listTitle="Youtube" />
         <MaterialList listTitle="Udemy" />
@@ -18,3 +26,7 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+const SContainer = styled.div`
+  text-align: right;
+`
