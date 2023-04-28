22989deafe2940097733a6551de9c011ce9e7358
kitsune_yk <agvksl543@gmail.com>
2023-04-28 18:56:59 +0900

    feat: login時recoilのatomにユーザ情報を保存

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,38 +1,56 @@
-import React, { useContext, useState, useEffect, useRef } from 'react'
-import { auth, db } from '../../lib/FirebaseConfig'
-import { onAuthStateChanged } from 'firebase/auth'
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../../lib/FirebaseConfig';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useSetRecoilState } from 'recoil';
+import { signInUserState } from '@/store/Auth/auth';
+
 
 // コンテキストを作成
-const AuthContext = React.createContext()
+const AuthContext = React.createContext();
 
 export function useAuth() {
     // useContextで作成したコンテキストを呼び出す
-    return useContext(AuthContext)
+    return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState(null)
-    const [loading, setLoading] = useState(true)
+    const [currentUser, setCurrentUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const setSignInUser = useSetRecoilState(signInUserState);
 
     // 第2引数に[]を指定して、初回レンダリングのみ関数を実行させる
     useEffect(() => {
         // onAuthStateChangedでログインの状態を監視する
-        const unsubscribe = onAuthStateChanged(auth, async user => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
             // ユーザー情報をcurrentUserに格納する
-            setCurrentUser(user)
-            setLoading(false)
-        })
-        return unsubscribe
-    }, [])
+            setCurrentUser(user);
+            setLoading(false);
+
+            // ログイン中のユーザー情報をsignInUserStateに保存する
+            if (user) {
+                setSignInUser({
+                    uid: user.uid,
+                    accessToken: user.accessToken,
+                });
+            } else {
+                // ログアウトしている場合はsignInUserStateを初期化する
+                setSignInUser({
+                    uid: null,
+                    accessToken: null,
+                });
+            }
+        });
+        return unsubscribe;
+    }, []);
 
     const value = {
-        currentUser
-    }
+        currentUser,
+    };
 
     // _app.jsで全コンポーネントをラッピングするためのプロバイダー
     return (
         <AuthContext.Provider value={value}>
             {!loading && children}
         </AuthContext.Provider>
-    )
+    );
 }
