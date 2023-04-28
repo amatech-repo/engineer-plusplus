3da61fdd0ed1eaee55030c08a0695489d80188b2
matsuo1234567 <122419523+matsuo1234567@users.noreply.github.com>
2023-04-29 04:52:29 +0900

    GoogleとGithub認証の追加

--- frontend/src/pages/auth/login.js
+++ frontend/src/pages/auth/login.js
@@ -1,4 +1,5 @@
 import styles from '../../styles/Home.module.css'
+import { GoogleAuthProvider, GithubAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Col, Container, Form, FormGroup, Input, Label, Row, Button } from "reactstrap";
 import { useState } from 'react';
@@ -11,6 +12,42 @@ export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter();
+  const auth = getAuth();
+  // Google 認証の処理
+  const handleGoogleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // Google 認証に成功した場合の処理
+        console.log(result);
+        if (router.pathname !== '/') {
+          router.push('/');
+        }
+      })
+      .catch((error) => {
+        // Google 認証に失敗した場合の処理
+        console.log(error);
+      });
+  };
+
+  // GitHub 認証の処理
+  const handleGithubSignIn = () => {
+    const provider = new GithubAuthProvider();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // GitHub 認証に成功した場合の処理
+        console.log(result);
+        if (router.pathname !== '/') {
+          router.push('/');
+        }
+      })
+      .catch((error) => {
+        // GitHub 認証に失敗した場合の処理
+        console.log(error);
+      });
+  };
 
   const doLogin = () => {
     const auth = getAuth();
@@ -71,6 +108,29 @@ export default function Login() {
             ログイン
           </Button>
         </Form>
+      </div>
+      <div style={{ marginTop: '16px' }}>
+        <Button
+          style={{ width: 220, marginRight: '16px' }}
+          color="primary"
+          onClick={() => {
+            // Google認証を行う関数を呼び出す
+            handleGoogleSignIn();
+
+          }}
+        >
+          Googleで登録
+        </Button>
+        <Button
+          style={{ width: 220 }}
+          color="primary"
+          onClick={() => {
+            // Github認証を行う関数を呼び出す
+            handleGithubSignIn();
+          }}
+        >
+          Githubで登録
+        </Button>
       </div>
       <div>
 
