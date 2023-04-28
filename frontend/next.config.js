126f2545595d4ed3ce3408143b895424af662e4f
kami5kaze <kusoerooyajikamiyama@gmail.com>
2023-04-28 13:50:46 +0900

    feat:プロフィール登録画面の追加

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,5 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "fs": false,
+        "tls": false,
+        "net": false,
+        "path": false,
+        "zlib": false,
+        "http": false,
+        "https": false,
+        "stream": false,
+        "crypto": false,
+      };
+    }
+
+    return config;
+  },
+};
 
 module.exports = nextConfig
---
+++ frontend/src/pages/profile/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layouts/layout"
+const Profile = () => {
+    return (
+        <Layout>
+            <h1>Profile list</h1>
+        </Layout>
+    )
+  }
+  
+  export default Profile
